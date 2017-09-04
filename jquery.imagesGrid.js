623341daf9b107f14c17d75776b57b4db40ea957
Lynesth <lyn@nautile.nc>
2017-09-04 20:59:14 +1100

    New version
    Added some new options *maxLines*, *showLastLine* and *alignLastLine*

--- jquery.imagesGrid.js
+++ jquery.imagesGrid.js
@@ -7,8 +7,13 @@
 			rowHeight: 150,
 			margin: 1,
 			imageSelector: '.image-thumb',
+			maxLines: false,
+			showLastLine: true,
+			alignLastLine: 'left',
 			responsive: true
 		}, options);
+
+		this.$el.css({paddingBottom: this.options.margin, textAlign: 'justify', fontSize: 0, lineHeight: 0, textAlignLast: this.options.alignLastLine});
 
 		if (this.options.responsive) {
 			var self = this;
@@ -28,7 +33,8 @@
 			totalWidth = 0,
 			containerWidth = this.$el.width(),
 			rowHeight = parseInt(this.options.rowHeight, 10),
-			border = parseInt(this.options.margin, 10);
+			margin = parseInt(this.options.margin, 10),
+			lines = 0;
 
 		this.$el.children().each(function() {
 			var $this = $(this),
@@ -37,29 +43,31 @@
 			if (thisHeight !== rowHeight) {
 				thisWidth = Math.floor(thisWidth * (rowHeight / thisHeight));
 			}
-			thisWidth += border;
-
-			if (totalWidth + thisWidth > containerWidth) {
-				totalWidth -= border;
-				var ratio = containerWidth / totalWidth,
-					newHeight = Math.floor(rowHeight * ratio) + 1,
-					newTotalWidth = 0,
-					len = row.length;
-				if (len > 1) {
-					while (newTotalWidth > containerWidth || newTotalWidth == 0) {
-						newHeight--;
-						newTotalWidth = 0;
-						while (len--) {
-							newTotalWidth += row[len].width * newHeight / row[len].height;
-						}
+			thisWidth += margin;
+			if (self.options.maxLines === false || lines < self.options.maxLines) {
+				if (totalWidth + thisWidth > containerWidth) {
+					lines++;
+					totalWidth -= margin;
+					var ratio = containerWidth / totalWidth,
+						newHeight = Math.floor(rowHeight * ratio) + 1,
+						newTotalWidth = 0,
 						len = row.length;
+					if (len > 1) {
+						while (newTotalWidth > containerWidth || newTotalWidth == 0) {
+							newHeight--;
+							newTotalWidth = 0;
+							while (len--) {
+								newTotalWidth += Math.ceil(row[len].width * newHeight / row[len].height);
+							}
+							len = row.length;
+						}
 					}
+					row.forEach(function(o) {
+						o.el.css({marginTop: margin, marginRight: 0, display: "inline-block"}).children(self.options.imageSelector).css({height: newHeight, maxHeight: 'none'});
+					});
+					row = [];
+					totalWidth = 0;
 				}
-				row.forEach(function(o) {
-					o.el.css('marginRight', 0).children(self.options.imageSelector).css({height: newHeight, maxHeight: 'none'});
-				});
-				row = [];
-				totalWidth = 0;
 			}
 
 			row.push({
@@ -71,7 +79,7 @@
 		});
 
 		row.forEach(function(o) {
-			o.el.css('marginRight', border).children(self.options.imageSelector).css({height: 'auto', maxHeight: rowHeight});
+			o.el.css({marginTop: margin, marginRight: margin, display: (self.options.showLastLine && self.options.maxLines === false) ? "inline-block" : "none"}).children(self.options.imageSelector).css({height: 'auto', maxHeight: rowHeight});
 		});
 	}
 
